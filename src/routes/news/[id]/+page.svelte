ecd958ef5fbbbeb2674cf4416382d82bab909096
Rafael Basso <rafaelbasso2008@hotmail.com>
2024-07-27 20:56:16 +0200

    reading view (feat): create page.

--- src/routes/news/[id]/+page.svelte
+++ src/routes/news/[id]/+page.svelte
@@ -5,8 +5,78 @@
 </script>
 
 {#if article}
-	<img src={article.bannerUrl} alt="banner" />
-	<h1>{article.title}</h1>
-	<p>{article.publishTime}</p>
-	<p>{article.content}</p>
+	<div class="article">
+		{#if article.bannerUrl}
+			<img src={article.bannerUrl} alt="banner" />
+		{/if}
+		<div class="article-content">
+			<h1>{article.title}</h1>
+			<p id="timestamp">{article.publishTime}</p>
+			<p id="content">{article.content}</p>
+		</div>
+	</div>
 {/if}
+
+<style lang="scss">
+	.article {
+		display: flex;
+		flex-direction: column;
+
+		img {
+			width: 100%;
+			aspect-ratio: 2 / 1;
+			object-fit: cover;
+		}
+
+		&-content {
+			display: flex;
+			flex-direction: column;
+
+			gap: 1em;
+			padding: 2em 2em;
+
+			* {
+				margin: 0;
+			}
+
+			h1 {
+				font-size: 2.5em;
+			}
+
+			#timestamp {
+				font-size: 1.4em;
+				color: var(--font-color-secondary);
+			}
+
+			#content {
+				border-top: 2px solid var(--separator-color);
+				padding: 2em 0;
+
+				font-size: 1.8em;
+				color: var(--font-color-primary);
+			}
+		}
+	}
+
+	@media screen and (min-width: 426px) {
+		.article {
+			&-content {
+				#content {
+					font-size: 2em;
+				}
+			}
+		}
+	}
+
+	@media screen and (min-width: 769px) {
+		.article {
+			max-width: 768px;
+
+			&-content {
+				#content {
+					font-size: 2.4em;
+				}
+			}
+		}
+	}
+</style>
