67b5f8beb5b9a8491601044387392126882aa99f
Rafael Basso <rafaelbasso2008@hotmail.com>
2024-07-27 17:04:07 +0200

    components (feat): create 'NewsCollection' component.

---
+++ src/lib/components/NewsCollection.svelte
@@ -0,0 +1,92 @@
+<script lang="ts">
+	import NewsModel from '$lib/models/NewsModel';
+	export let news: NewsModel[];
+</script>
+
+<div class="news">
+	{#each news as news}
+		<div class="news-article">
+			<div class="news-article-content">
+				<div class="news-article-content-info">
+					<p class="author">{news.author}</p>
+					<p class="title">{news.title}</p>
+				</div>
+				<div class="news-article-content-image">
+					<img src={news.bannerUrl} alt="news" />
+				</div>
+			</div>
+			<div class="news-article-footer">
+				<span class="publish-time">{news.publishTime}</span>
+			</div>
+		</div>
+	{/each}
+</div>
+
+<style lang="scss">
+	.news {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		gap: 1em;
+
+		font-size: 1.6em;
+		padding: 1em 2em;
+	}
+
+	.news-article {
+		display: flex;
+		flex-direction: column;
+		gap: 1em;
+
+		width: 100%;
+		padding: 1em;
+
+		border-bottom: 2px solid var(--separator-color);
+
+		&-content {
+			display: flex;
+			gap: 1em;
+
+			&-info {
+				display: flex;
+				flex-direction: column;
+				justify-content: space-evenly;
+				gap: 1em;
+				flex-grow: 2;
+
+				* {
+					margin: 0;
+				}
+
+				.author {
+					font-weight: bold;
+				}
+
+				.title {
+					flex-grow: 2;
+					font-size: 1.3em;
+				}
+			}
+
+			&-image {
+				display: flex;
+				align-items: center;
+				max-width: 20%;
+
+				img {
+					width: 100%;
+					border-radius: 0.5em;
+					aspect-ratio: 1 / 1;
+					object-fit: cover;
+				}
+			}
+		}
+
+		&-footer {
+			.publish-time {
+				font-size: 0.9em;
+				color: var(--font-color-secondary);
+			}
+		}
+	}
+</style>
---
+++ src/lib/models/NewsModel.ts
@@ -0,0 +1,21 @@
+export default class News {
+	title: string;
+	author: string;
+	content: string;
+	publishTime: string;
+	bannerUrl: string;
+
+	constructor(
+		title: string,
+		author: string,
+		content: string,
+		publishTime: string,
+		bannerUrl: string = ''
+	) {
+		this.title = title;
+		this.author = author;
+		this.content = content;
+		this.publishTime = publishTime;
+		this.bannerUrl = bannerUrl;
+	}
+}
