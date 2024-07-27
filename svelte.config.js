0a71188e551555bf5ea6a945119e7b7a20a2e7f6
Rafael Basso <rafaelbasso2008@hotmail.com>
2024-07-27 18:08:31 +0200

    svelte.config (chore): fix broken alias paths.

--- svelte.config.js
+++ svelte.config.js
@@ -13,8 +13,8 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
 		alias: {
-			components: '$lib/components',
-			models: '$lib/models'
+			$components: 'src/lib/components',
+			$models: 'src/lib/models'
 		}
 	}
 };
