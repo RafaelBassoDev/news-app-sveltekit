a76378065f32be504ca39fb22abd69d179694f0a
Rafael Basso <rafaelbasso2008@hotmail.com>
2024-07-27 17:53:49 +0200

    components (feat): create 'NewsArticle' component.

---
+++ src/lib/components/NewsArticle.svelte
@@ -0,0 +1,78 @@
+<script lang="ts">
+	import NewsModel from '$lib/models/NewsModel';
+	export let article: NewsModel;
+</script>
+
+<div class="news-article">
+	<div class="news-article-content">
+		<div class="news-article-content-info">
+			<p class="author">{article.author}</p>
+			<p class="title">{article.title}</p>
+		</div>
+		<div class="news-article-content-image">
+			<img src={article.bannerUrl} alt="news" />
+		</div>
+	</div>
+	<div class="news-article-footer">
+		<span class="publish-time">{article.publishTime}</span>
+	</div>
+</div>
+
+<style lang="scss">
+	.news-article {
+		display: flex;
+		flex-direction: column;
+		gap: 1em;
+
+		width: 100%;
+		padding: 1em;
+
+		border-bottom: 2px solid var(--separator-color);
+
+		&-content {
+			display: flex;
+			gap: 1em;
+
+			&-info {
+				display: flex;
+				flex-direction: column;
+				justify-content: space-evenly;
+				gap: 1em;
+				flex-grow: 2;
+
+				* {
+					margin: 0;
+				}
+
+				.author {
+					font-weight: bold;
+				}
+
+				.title {
+					flex-grow: 2;
+					font-size: 1.3em;
+				}
+			}
+
+			&-image {
+				display: flex;
+				align-items: center;
+				max-width: 20%;
+
+				img {
+					width: 100%;
+					border-radius: 0.5em;
+					aspect-ratio: 1 / 1;
+					object-fit: cover;
+				}
+			}
+		}
+
+		&-footer {
+			.publish-time {
+				font-size: 0.9em;
+				color: var(--font-color-secondary);
+			}
+		}
+	}
+</style>
