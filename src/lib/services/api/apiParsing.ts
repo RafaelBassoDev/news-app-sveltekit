cc6b91880e01ebdd71fb22e88c880375c12b8df1
Rafael Basso <rafaelbasso2008@hotmail.com>
2024-07-28 14:23:27 +0200

    news (feat): add cached api fetching feature.

--- src/lib/services/api/apiParsing.ts
+++ src/lib/services/api/apiParsing.ts
@@ -1,20 +1,23 @@
 import type { NewsArticleModel } from '$models/NewsArticleModel';
 import type { APIResponse } from '$services/api/interfaces';
 import { cyrb53 } from '$services/hashing';
+import { format } from 'date-fns';
 
 function parse(response: APIResponse): NewsArticleModel[] {
 	const parsedData: NewsArticleModel[] = [];
 	const articles = response.articles;
 
 	articles.forEach((article) => {
-		parsedData.push({
-			id: cyrb53(article.title).toString(),
-			author: article.author,
-			title: article.title,
-			content: article.content,
-			publishTime: article.publishedAt,
-			bannerUrl: article.urlToImage
-		});
+		if (article.title !== '[Removed]') {
+			parsedData.push({
+				id: cyrb53(article.title).toString(),
+				author: article.author,
+				title: article.title,
+				content: article.content,
+				publishTime: format(article.publishedAt, 'p, dd/MM/yyyy'),
+				bannerUrl: article.urlToImage
+			});
+		}
 	});
 
 	return parsedData;
--- src/routes/news/+layout.ts
+++ src/routes/news/+layout.ts
@@ -1,16 +1,23 @@
 import type { NewsArticleModel } from '$models/NewsArticleModel';
 import { request, parseResponse, cache } from '$services/api';
+import { articles } from '$stores/articles';
+import { get } from 'svelte/store';
 
 export async function load() {
-	let articles: NewsArticleModel[] = [];
+	const cachedArticles: NewsArticleModel[] = get(articles);
+
+	if (cachedArticles.length > 0) {
+		return { articles: cachedArticles };
+	}
+
+	let fetchedArticles: NewsArticleModel[] = [];
 
 	await request('everything', ['q=technology', 'language=en', 'sortBy=popularity', 'pageSize=20'])
 		.then((res) => parseResponse(res))
 		.then((data) => {
 			cache(data);
-			console.log(data);
-			articles = data;
+			fetchedArticles = data;
 		});
 
-	return { articles };
+	return { articles: fetchedArticles };
 }
