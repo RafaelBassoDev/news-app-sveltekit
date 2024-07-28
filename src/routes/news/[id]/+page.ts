03e6437e7fb736df18fc07fcc43d4306d5625d44
Rafael Basso <rafaelbasso2008@hotmail.com>
2024-07-28 14:03:28 +0200

    newsArticle (refactor): add data fetching from cache.

--- src/routes/news/[id]/+page.ts
+++ src/routes/news/[id]/+page.ts
@@ -1,9 +1,11 @@
+import { articles } from '$stores/articles.js';
 import { error } from '@sveltejs/kit';
+import { get } from 'svelte/store';
 
-export async function load({ params, parent }) {
-	const { newsList } = await parent();
+export async function load({ params }) {
+	const cachedArticles = get(articles);
 
-	const article = newsList.find((article) => article.id === params.id);
+	const article = cachedArticles.find((article) => article.id === params.id);
 
 	if (!article) throw error(404);
 
